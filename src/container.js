a26f85f99aa03e5ea204d390b09840be38d19ca1
zinkey <yaya@uloveit.com.cn>
2016-04-28 14:28:15 +0800

    fix style

--- src/container.js
+++ src/container.js
@@ -2,16 +2,10 @@ import WeakMap from 'es6-weak-map';
 
 const container = new WeakMap();
 
-const get = (store) => {
-  return container.get(store);
-};
+const get = (store) => container.get(store);
 
-const set = (store, list) => {
-  container.set(store, list);
-};
+const set = (store, list) => container.set(store, list);
 
-const del = (store) => {
-  container.delete(store);
-};
+const del = (store) => container.delete(store);
 
 export { get, set, del };
