a8f801bdfe2a8c2ff469be81355ef4ba206e3478
zinkey <yaya@uloveit.com.cn>
2016-05-05 17:47:40 +0800

    fix ie8 default bug

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,14 @@ module.exports = function(webpackConfig) {
   webpackConfig.plugins.some(function(plugin, i){
     if(plugin instanceof webpack.optimize.CommonsChunkPlugin) {
       webpackConfig.plugins.splice(i, 1);
-
       return true;
     }
+  });
+
+  // Fix ie8 compatibility
+  webpackConfig.module.loaders.unshift({
+    test: /\.jsx?$/,
+    loader: 'es3ify-loader',
   });
 
   return webpackConfig;
